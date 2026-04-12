d2e3d93e76dacd896ca8c2cbd4420f14ce00ca22
ShashwatSolanki <shashwatsolanki786@gmail.com>
2026-04-12 17:37:37 +0530

    ChildProcess - chroot and namespace entry (container isolation, chroot, proc mounting, hostname)

--- runtime/runtime.go
+++ runtime/runtime.go
@@ -57,3 +57,56 @@ func RunIsolated(rootDir string, command []string, workdir string, envVars []str
 	}
 	return 0, nil
 }
+
+// ChildProcess is called when the binary is re-exec'd inside a new namespace.
+// It sets up chroot and mounts, then execs the actual command.
+func ChildProcess(args []string) error {
+	if len(args) < 4 {
+		return fmt.Errorf("child process requires at least 4 args: rootDir, workdir, mode, command...")
+	}
+
+	rootDir := args[0]
+	workdir := args[1]
+	mode := args[2]
+	cmdArgs := args[3:]
+
+	// Mount proc inside the new root.
+	procPath := filepath.Join(rootDir, "proc")
+	os.MkdirAll(procPath, 0755)
+	syscall.Mount("proc", procPath, "proc", 0, "")
+
+	// Chroot into the rootDir.
+	if err := syscall.Chroot(rootDir); err != nil {
+		return fmt.Errorf("chroot to %s: %w", rootDir, err)
+	}
+
+	// Change to the working directory.
+	if err := syscall.Chdir(workdir); err != nil {
+		return fmt.Errorf("chdir to %s: %w", workdir, err)
+	}
+
+	// Set hostname.
+	syscall.Sethostname([]byte("docksmith"))
+
+	// Execute the command.
+	var cmd *exec.Cmd
+	if mode == "shell" {
+		shellCmd := strings.Join(cmdArgs, " ")
+		cmd = exec.Command("/bin/sh", "-c", shellCmd)
+	} else {
+		cmd = exec.Command(cmdArgs[0], cmdArgs[1:]...)
+	}
+
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	cmd.Env = os.Environ()
+
+	if err := cmd.Run(); err != nil {
+		if exitErr, ok := err.(*exec.ExitError); ok {
+			os.Exit(exitErr.ExitCode())
+		}
+		return err
+	}
+	return nil
+}
